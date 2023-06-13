f1751424aff78bbc6e721a95072facc4a3240231
Marley Barrett <marleyhansenbarrett@icloud.com>
2023-06-13 14:16:24 -0500

    Added Axios configuration and connected mongo connection

--- server/server.js
+++ server/server.js
@@ -1,25 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-  res.send('Hello, MERN!');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mhb:ORMj2W3dakH0mFJZ@cluster0.18ctz4a.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
-
-mongoose.connect('mongodb://localhost/mydatabase', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log(err));
-
-  const admin = require('firebase-admin');
+  /* const admin = require('firebase-admin');
 
 const serviceAccount = require('./path/to/serviceAccountKey.json'); // Path to your service account key JSON file
 
@@ -29,4 +32,4 @@ admin.initializeApp({
 });
 
 const firestore = admin.firestore();
-
+*/
